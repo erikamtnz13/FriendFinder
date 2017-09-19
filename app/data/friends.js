55d81f03416b3786039fbae21fdaf82ca5a248d8
Erika Martinez <erikamartinez@Erikas-MacBook-Pro.local>
2017-09-19 15:50:31 -0700

    adding changes

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friends = [
+    {"name": "Beyonce",
+     "picture": "https://images-na.ssl-images-amazon.com/images/I/A15dQM39ELL._CR0,0,3840,2880_._SL1000_.jpg",
+     "scores": [
+         5,
+         3,
+         1,
+         3,
+         4,
+         1,
+         3,
+         4,
+         1,
+         1
+     ]
+    },
+    {"name": "Selena",
+     "picture": "https://pbs.twimg.com/profile_images/884533034669588480/am6x8vE4.jpg",
+     "scores": [
+         1,
+         1,
+         2,
+         2,
+         2,
+         1,
+         5,
+         5,
+         3,
+         1
+     ]
+    },
+    {"name": "James",
+     "picture": "http://i2.mirror.co.uk/incoming/article5200863.ece/ALTERNATES/s615b/Actor-James-Corden-of-One-Chance-poses-at-the-Guess-Portrait-Studio.jpg",
+     "scores": [
+         3,
+         4,
+         5,
+         2,
+         1,
+         1,
+         5,
+         3,
+         1,
+         1
+     ]
+    },
+    ];
+    
+    
+    
+    module.exports = friends;
